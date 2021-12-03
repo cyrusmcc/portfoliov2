51fd795778725e32ce39a6c379b04f039f507633
cyrusmcc <cyrusmcc@hotmail.com>
2021-12-03 11:38:44 -0700

    Change history mode to hash

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory  } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
@@ -20,8 +20,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  mode: "hash",
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory (process.env.BASE_URL),
   routes,
 });
 
